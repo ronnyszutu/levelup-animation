de1a56ba4b8ee233e5f4993b31b75c6d96404870
szuturon <ronnyszutu@gmail.com>
2022-05-15 16:32:22 -0400

    Full Drag Example

--- src/drag.js
+++ src/drag.js
@@ -1,6 +1,55 @@
+import { spring } from 'svelte/motion';
+
 export function drag(node, params) {
-    // node.style.background = "red";
-    node.addEventListener("mousedown", () => console.log("click"));
+    let x;
+
+    const coordinates = spring(
+        { x: 0 },
+        {
+            stiffness: 0.2,
+            damping: 0.4,
+        }
+    )
+
+    coordinates.subscribe(( $coords ) => {
+        node.style.transform =  `translate3d(${$coords.x}px, 0, 0)`;
+    })
+
+    node.addEventListener("mousedown", handleMouseDown);
+
+    function handleMouseDown(event) {
+        x = event.clientX;
+        window.addEventListener("mousemove", handleMouseMove);
+        window.addEventListener("mouseup", handleMouseUp);
+    }
+
+    function handleMouseMove(event) {
+        // Delta X = difference of where we clicked vs where we are currently
+        const dx = event.clientX - x;
+        x = event.clientX;
+        coordinates.update(($coords) => {
+            return {
+                x: $coords.x + dx
+            }
+        })
+        console.log(event.clientY, event.clientX);
+
+    }
+
+    function handleMouseUp() {
+        // Reset values
+        x = 0;
+        coordinates.update(() => {
+            return {
+                x: 0,
+            }
+        })
+        
+        // Remove event listeners
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("mouseup", handleMouseUp);
+    }
+
     return {
         destroy() {
             node.removeEventListener("mousedown", () => console.log("click"));
