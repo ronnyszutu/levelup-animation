0411f108a8a8bf07af259202704d61887bd90cd8
szuturon <ronnyszutu@gmail.com>
2022-05-14 23:36:51 -0400

    Project 3 - Modal

--- src/App.svelte
+++ src/App.svelte
@@ -3,21 +3,27 @@
 	import { custom } from './custom';
 	import Nav from './Nav.svelte';
 	import Toast from './Toast.svelte';
+	import Modal from './Modal.svelte';
 	import { alert } from "./alert";
 	export let name;
 
 	let isReady = 'false';
 	let isNavOpen = false;
+	let isModalOpen = false;
 
 	function toggleNav() {
 		isNavOpen = !isNavOpen;
 	}
-
+	
 	function toggleAlert() {
 		alert.set({
 			isActive: true,
 			text: "Our new alert",
 		})
+	}
+
+	function toggleModal() {
+		isModalOpen = !isModalOpen;
 	}
 </script>
 
@@ -29,6 +35,7 @@
 <main>
 	<button on:click={ toggleNav }>Menu</button>
 	<button on:click={ toggleAlert }>Alert</button>
+	<button on:click={ toggleModal }>Modal</button>
 
 	{#if isReady}
 		<h1 transition:custom={{ delay: 2000 }}>Hello {name}!</h1>
@@ -38,6 +45,14 @@
 </main>
 
 <Toast />
+
+{#if isModalOpen}
+	<!-- content here -->
+	<Modal {toggleModal}>
+		<p>This is in the modals</p>
+		<button>Do something</button>
+	</Modal>
+{/if}
 
 <style>
 	.hidden {
---
+++ src/Modal.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { scale, fly } from 'svelte/transition';
+    export let toggleModal;
+</script>
+<div class="modal">
+    <div class="card" transition:fly={{ y:40 }}>
+        <button on:click={toggleModal} class="drawer--close">X</button>
+        <slot />
+    </div>
+    <div on:click={toggleModal} transition:scale={{ start: 1.5, duration: 1000 }} class="background"></div>
+</div>
+
+
+<style>
+    .modal {
+        position: fixed;
+        display: flex;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .card {
+        position: relative;
+        text-align: center;
+    }
+
+    .background {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+        background: linear-gradient(142deg, var(--red) 3.62%, var(--green) 100%);
+        opacity: 0.8;
+        /* background: rgba(0,0,0,0.8); */
+    }
+</style>
